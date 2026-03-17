11357b8cfb7999c70a45a2ddfbc1a9d9ffd5ef75
zeter <retez94@gmail.com>
2026-03-17 21:54:01 +0100

    Initial Commit

---
+++ GRMerge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GRMerge
+//
+//  Created by Giorgio Retez on 17/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GRMerge/GRMergeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GRMergeApp.swift
+//  GRMerge
+//
+//  Created by Giorgio Retez on 17/03/26.
+//
+
+import SwiftUI
+
+@main
+struct GRMergeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
